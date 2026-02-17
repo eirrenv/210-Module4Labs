c6050062721cd510a3f9de43ad3254748c71c270
Eirren <edv2019@gmail.com>
2026-02-17 11:39:31 -0800

    Created function prototypes and basic main code for inputting and displaying players

--- Lab11.cpp
+++ Lab11.cpp
@@ -1,19 +1,20 @@
 /*
-    Creating a list of players on a basketball team (my favorite is the Toronto Raptors :))
+    Creating a list of players on a basketball team [my favorite is the Toronto Raptors :)]
 */
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-// array constant for keeping points over 5 games
-const int games = 5;
+// array constant for amount of players and games counted
+const int PLAYERS = 5, GAMES = 5;
 
 // struct for basketball team
 struct Team {
     string playerName;
     int playerNum;
     int * ptsOverGames;
+    int ptsAvg;
 
     ~Team() {
         if (ptsOverGames)
@@ -22,7 +23,26 @@ struct Team {
     }
 };
 
+// protoypes for player input and display
+void inputPlayer(Team *);
+void displayPlayer(Team *);
+
 int main() {
+    // new team roster array for starting 5 on team
+    Team *roster = new Team[PLAYERS];
+
+    // player input and printing starting 5
+    for (int i = 0; i < PLAYERS; ++i) {
+        inputPlayer(&roster[i]);
+    }
+
+    for (int i = 0; i < PLAYERS; ++i) {
+        displayPlayer(&roster[i]);
+    }
 
     return 0;
+}
+
+void inputPlayer(Team * player) {
+
 }

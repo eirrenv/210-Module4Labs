dacd59ec90f346538af11cddb46b67a85430e2df
Eirren <edv2019@gmail.com>
2026-02-17 11:54:37 -0800

    Created inputPlayer function and tested code, commented out displayPlayer function for next commit

--- Lab11.cpp
+++ Lab11.cpp
@@ -13,8 +13,9 @@ const int PLAYERS = 5, GAMES = 5;
 struct Team {
     string playerName;
     int playerNum;
+    string playerPos;
     int * ptsOverGames;
-    int ptsAvg;
+    double ptsAvg;
 
     ~Team() {
         if (ptsOverGames)
@@ -25,7 +26,7 @@ struct Team {
 
 // protoypes for player input and display
 void inputPlayer(Team *);
-void displayPlayer(Team *);
+// void displayPlayer(Team *);
 
 int main() {
     // new team roster array for starting 5 on team
@@ -36,13 +37,36 @@ int main() {
         inputPlayer(&roster[i]);
     }
 
-    for (int i = 0; i < PLAYERS; ++i) {
-        displayPlayer(&roster[i]);
-    }
+    // for (int i = 0; i < PLAYERS; ++i) {
+    //    displayPlayer(&roster[i]);
+    // }
 
     return 0;
 }
 
-void inputPlayer(Team * player) {
 
+// function to input data for players, inputting name, number, position, PPG, and calculating PPG average over 5 game stretch
+void inputPlayer(Team * player) {
+    static int numPlayer = 1;
+    cout << "Input data for player #" << numPlayer << ":\n";
+    cout << "Player name: ";
+    getline(cin, player->playerName);
+    cout << "Player number: ";
+    cin >> player->playerNum;
+    cin.ignore();
+    cout << "Player position: ";
+    getline(cin, player->playerPos);
+
+    player->ptsAvg = 0.0;
+    player->ptsOverGames = new int[GAMES];
+    cout << "Player points over games:" << endl;
+    for (int i = 0; i < GAMES; ++i) {
+        cout << "Game #" << i + 1 << ": ";
+        cin >> player->ptsOverGames[i];
+        player->ptsAvg += player->ptsOverGames[i];
+    }
+    player->ptsAvg = player->ptsAvg / 5.0;
+    cin.ignore();
+    cout << endl << endl;
+    numPlayer++;
 }

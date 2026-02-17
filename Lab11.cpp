442a0800903b15735f0a470f794ee6b1fd3a2d50
Eirren <edv2019@gmail.com>
2026-02-17 11:33:15 -0800

    Created Team struct

--- Lab11.cpp
+++ Lab11.cpp
@@ -1,7 +1,28 @@
+/*
+    Creating a list of players on a basketball team (my favorite is the Toronto Raptors :))
+*/
+
 #include <iostream>
+#include <string>
 using namespace std;
 
+// array constant for keeping points over 5 games
+const int games = 5;
+
+// struct for basketball team
+struct Team {
+    string playerName;
+    int playerNum;
+    int * ptsOverGames;
+
+    ~Team() {
+        if (ptsOverGames)
+            delete [] ptsOverGames;
+        ptsOverGames = nullptr;
+    }
+};
+
 int main() {
-    
+
     return 0;
 }

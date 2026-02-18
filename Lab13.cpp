cd21d96fee68a8a2495adc15450ebb4961a146b6
Eirren <edv2019@gmail.com>
2026-02-17 16:01:24 -0800

    changed array calls into vector calls, running into issues but commiting

--- Lab13.cpp
+++ Lab13.cpp
@@ -3,7 +3,7 @@
 */
 
 #include <iostream>
-#include <array>
+#include <vector>
 #include <string>
 #include <algorithm> // for sort(), find()
 #include <numeric> // for accumulate()
@@ -11,18 +11,18 @@
 #include <fstream>
 using namespace std;
 
-// 30 days in a month, the SIZE of the array
+// 30 days in a month, the SIZE of the vector
 const int SIZE = 30;
 
 int main() {
-    // initialize array
-    array<double, SIZE> balance;
+    // initialize vector
+    vector<double> balance (SIZE);
 
     // open txt file, check for any errors
     ifstream fin;
     fin.open("finances.txt");
     if(fin.good()) {
-        // placing values from file into array
+        // placing values from file into vector
         for (int i = 0; i < SIZE; ++i) {
             fin >> balance.at(i);
         }
@@ -32,7 +32,7 @@ int main() {
         cout << "File not found.\n";
     }
 
-    // sorting array from highest to lowest
+    // sorting vector from highest to lowest
     sort(balance.rbegin(), balance.rend());
     cout << "Sorted Balance: ";
     for (double val : balance) 
@@ -50,9 +50,9 @@ int main() {
 
     cout << endl;
 
-    // using iterator to find value in array
+    // using iterator to find value in vector
     double target = 653.64;
-    array<double, SIZE> :: iterator it;
+    vector<double>::iterator it;
     it = find(balance.begin(), balance.end(), target);
     if (it != balance.end()) {
         cout << target << " Found in position " << it - balance.begin() << endl;
@@ -70,8 +70,7 @@ int main() {
     cout << endl;
     
     // sorted balance, without duplicate values
-    array<double, SIZE> sortedBalance;
-    sortedBalance.fill(0.0);
+    vector<double> sortedBalance(SIZE);
     sortedBalance.at(0) = balance.at(0);
     int j = 0;
     for (int i = 0; i < SIZE; ++i) {
@@ -82,12 +81,11 @@ int main() {
         sortedBalance.at(j) = balance.at(i);
     }
     
-    // sortedBalance array sorted from lowest to highest
-    cout << "Sorted balance array from lowest to highest value:" << endl;
-    for (int i = 0; sortedBalance.at(i) != 0.0; ++i) {
+    // sortedBalance vector sorted from lowest to highest
+    cout << "Sorted balance vector from lowest to highest value:" << endl;
+    for (int i = 0; i < SIZE; ++i) {
         cout << "$" << sortedBalance.at(i) << " ";
     }
-    cout << endl;
 
     cout << endl;
 
